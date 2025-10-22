4b66e53d96d13f66e7523767e029e18cc2c90825
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-22 14:21:24 +0200

    feat: add brand bar with perfume logos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,11 @@
 import { Header } from "../components/Header"
 import { useEffect } from "react"
+import chanelLogo from "../assets/brands/chanell.png"
+import versaceLogo from "../assets/brands/versace.webp"
+import armaniLogo from "../assets/brands/armani.png"
+import diorLogo from "../assets/brands/dior.png"
+import gucciLogo from "../assets/brands/gucci.png"
+import "./HomePage.css"
 export function HomePage(){
     useEffect(() => {
         document.title = "Vintiada"
@@ -7,7 +13,23 @@ export function HomePage(){
     return (
         <>
         <Header></Header>
-            
+            <div className="brands-container">
+                <div className="brand">
+                    <img src={chanelLogo}></img>
+                </div>
+                <div className="brand">
+                    <img src={versaceLogo}></img>
+                </div>
+                <div className="brand">
+                    <img src={armaniLogo}></img>
+                </div>
+                <div className="brand">
+                    <img src={diorLogo}></img>
+                </div>
+                <div className="brand">
+                    <img src={gucciLogo}></img>
+                </div>
+            </div>
         </>
     )
 }
