f1ddc6820966429ad649a2091cf6a6071a884aba
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-22 15:10:42 +0200

    feat: add onClick function to change Advertisement

--- src/pages/Advertisements.jsx
+++ src/pages/Advertisements.jsx
@@ -1,21 +1,32 @@
 import adv1Big from "../assets/adv-1big.png"
+import adv2Big from "../assets/adv-2big.png"
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import { useState } from "react";
 import "./Advertisements.css"
 export function Adv(){
-    
+    const [advImage, setAdvImage] = useState(adv1Big);
+
+    function changeImage(){
+        if(advImage === adv1Big){
+            setAdvImage(adv2Big)
+        } else {
+            setAdvImage(adv1Big)
+        }
+    }
 
     return (
         <section>
-            <img src={adv1Big}></img>
+            <img src={advImage}></img>
             <div className="l-arrow">
                 <FaArrowLeft 
-                
+                onClick={changeImage}
                 className="arrow"/>
             </div>
             <div className="right-arrow">
-                <FaArrowRight className="arrow"/>
+                <FaArrowRight 
+                onClick={changeImage}
+                className="arrow"/>
             </div>
         </section>
     )
