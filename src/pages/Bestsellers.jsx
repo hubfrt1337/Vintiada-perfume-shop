8b656605d95d8d6e233d8b5d9163b1e79b8c85ed
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-22 20:43:45 +0200

    move prefumes.json into public folder and fetch data

--- src/pages/Bestsellers.jsx
+++ src/pages/Bestsellers.jsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react"
 import "./Bestsellers.css"
 export function BestSellers(){
+    useEffect(() => {
+        const fetchPerfumes = async () => {
+            const response = await fetch('/perfumes.json')
+            const data = await response.json()
+            console.log(data)
+        }
+        fetchPerfumes()
+    }, [])
     return (
         <div className="best-sellers-container">
             <div className="title-best">Best Sellers</div>
