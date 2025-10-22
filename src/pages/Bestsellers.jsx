313c1aff157322ae1917e2d6c0c2f9b657220f58
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-22 21:05:06 +0200

    feature: shuffle and reduce data to bestsellers products only.

--- src/pages/Bestsellers.jsx
+++ src/pages/Bestsellers.jsx
@@ -1,17 +1,28 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import "./Bestsellers.css"
 export function BestSellers(){
+    const [perfumes, setPerfumes] = useState([]);
     useEffect(() => {
         const fetchPerfumes = async () => {
             const response = await fetch('/perfumes.json')
             const data = await response.json()
-            console.log(data)
+            setPerfumes(data)
         }
         fetchPerfumes()
     }, [])
+    const bestsellers = perfumes.filter(perfum => perfum.bestseller)
+    .sort(() => 0.5 - Math.random()).slice(0, 6)
+    
     return (
         <div className="best-sellers-container">
             <div className="title-best">Best Sellers</div>
+            <div className="perfumes-container">
+                {bestsellers.map(perfum => {
+                    return (
+                        <div key={perfum.id}>{perfum.name}</div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
