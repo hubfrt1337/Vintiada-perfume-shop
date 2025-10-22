108fa37cf9dfd77861fe1cda22f09c76b4b990cd
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-23 01:13:46 +0200

    add perfum info

--- src/pages/Bestsellers.jsx
+++ src/pages/Bestsellers.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import "./Bestsellers.css"
-export function BestSellers(){
+export function BestSellers() {
     const [perfumes, setPerfumes] = useState([]);
     useEffect(() => {
         const fetchPerfumes = async () => {
@@ -11,19 +11,25 @@ export function BestSellers(){
         fetchPerfumes()
     }, [])
     const bestsellers = perfumes.filter(perfum => perfum.bestseller)
-    .sort(() => 0.5 - Math.random()).slice(0, 6)
-    
+        .sort(() => 0.5 - Math.random()).slice(0, 6)
+
     return (
         <div className="best-sellers-container">
             <div className="title-best">Best Sellers</div>
             <div className="perfumes-container">
                 {bestsellers.map(perfum => {
                     return (
-                        <div key={perfum.id}>
-                        <div className="image-container">
-                            <img src={perfum.image}></img>
-                        </div>
-                        <div key={perfum.id}>{perfum.name}</div>
+                        <div className="flex-best" key={perfum.id}>
+                            <div className="image-container">
+                                <img src={perfum.image}></img>
+                            </div>
+                            <div className="perfum-brand">{perfum.brand}</div>
+                            <div className="perfum-name">{perfum.name}</div>
+                            <div className="perfum-type">{perfum.gender === "unisex"
+                                ? "Perfume for Everyone"
+                                : perfum.gender === "feminine"
+                                    ? "Perfume for women"
+                                    : "Perfume for men"}</div>
                         </div>
                     )
                 })}
