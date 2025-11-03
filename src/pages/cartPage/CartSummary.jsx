a83ed73a172739f9895214d615961c93f62da21a
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-11-03 19:04:57 +0100

    feat: show removed message

--- src/pages/cartPage/CartSummary.jsx
+++ src/pages/cartPage/CartSummary.jsx
@@ -2,6 +2,7 @@ import { showAddedMessage } from "../../utils/showMessage";
 import { useRef } from "react";
 export function CartSummary({c, handleAddToCart, handleSubtractCart, deleteProduct}) {
             const addRef = useRef();
+            const remRef = useRef();
             return (
                 <div key={c.id} className="product-page-info">
                     <div className="page-image-container">
@@ -16,13 +17,15 @@ export function CartSummary({c, handleAddToCart, handleSubtractCart, deleteProdu
                         <div className="product-page-price">{(c.price / 100).toFixed(2)}$</div>
                         <div className="quantity-container relat">
                             <button onClick={() => {
-                                handleSubtractCart(c)
+                                handleSubtractCart(c);
+                                showAddedMessage(remRef, "removed")
                             }} className="minus">-</button>
                             <span className="quantity">{c.quantity}</span>
                             <button
                                 onClick={() => { handleAddToCart(c); showAddedMessage(addRef) }}
                                 className="plus">+</button>
                             <div ref={addRef} className="added-message absolut">Added</div>
+                            <div ref={remRef} className="removed-message absolut">Removed</div>
                         </div>
                         <div onClick={() => { deleteProduct(c) }} className="delete">Delete</div>
                     </div>
--- src/utils/showMessage.js
+++ src/utils/showMessage.js
@@ -1,7 +1,13 @@
-export const showAddedMessage = (addRef) => {
+export const showAddedMessage = (addRef, message) => {
+    if (message === "removed") {
         addRef.current.classList.add("add-show")
-        console.log(addRef.current)
+        setTimeout(() => {
+            addRef.current.classList.remove("add-show")
+        }, 1500)
+    } else {
+        addRef.current.classList.add("add-show")
         setTimeout(() => {
             addRef.current.classList.remove("add-show")
         }, 1500)
     }
+}
