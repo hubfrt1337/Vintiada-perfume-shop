8405fe03d3e93ca0ae1a660bc9a851c06e33e9b2
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-11-03 20:38:52 +0100

    feat: add links to each product page

--- src/components/RenderPerfumes.jsx
+++ src/components/RenderPerfumes.jsx
@@ -1,23 +1,25 @@
+import { Link } from "react-router"
 export function RenderPerfumes({ perfumes, type }) {
     return (
         <section className="products-flex">
             {perfumes.filter(perfum => perfum.gender === type)
                 .map(perfum => {
                     return (
-                        <div className="flex-best box" key={perfum.id}>
-                            <div className="image-container">
-                                <img src={perfum.image}></img>
-                            </div>
-                            <div className="perfum-brand">{perfum.brand}</div>
-                            <div className="perfum-name">{perfum.name}</div>
-                            <div className="perfum-type">Perfume for 
-                                {perfum.gender === "masculine" ? " Men" :
-                                perfum.gender === "feminine" ? " Women" 
-                            : " Everyone"}
-                                
-                            </div>
-                        </div>
+                        <Link to={`/product/${perfum.id}`} key={perfum.id} style={{ textDecoration: "none" }}>
+                            <div className="flex-best box" >
+                                <div className="image-container">
+                                    <img src={perfum.image}></img>
+                                </div>
+                                <div className="perfum-brand">{perfum.brand}</div>
+                                <div className="perfum-name">{perfum.name}</div>
+                                <div className="perfum-type">Perfume for
+                                    {perfum.gender === "masculine" ? " Men" :
+                                        perfum.gender === "feminine" ? " Women"
+                                            : " Everyone"}
 
+                                </div>
+                            </div>
+                        </Link>
                     )
                 })}
         </section>
