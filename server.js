79f4f7332da53a23386dcb963197721802f3e6f9
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-11-08 09:35:22 +0100

    fix: srerver CORS policy

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ import fs from 'fs/promises';
 import path from 'path';
 
 
+app.use((req, res, next) => {
+          res.header('Access-Control-Allow-Origin', 'https://vintiada-perfume-shop.onrender.com'); // Replace with your actual frontend domain
+          res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+          res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+          if (req.method === 'OPTIONS') {
+            return res.sendStatus(200);
+          }
+          next();
+        });
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
